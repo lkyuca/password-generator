c0814675b6cc62b879e2c6431b8fd5b70e22d2ea
lkyuca <74152278+lkyuca@users.noreply.github.com>
2026-06-11 23:41:19 +0200

    Delete wachtwoord generator 1.py

--- wachtwoord generator 1.py
+++
@@ -1,8 +0,0 @@
-print ("welcome gebruiker")
-import random
-tekens  = ("abcdefghijklmnopqurstvxyzABCDEFGHIJKLMNOPQURSTVXYZ1234567890!@#$%^&*")
-hoeveel =int(input("hoeveel wachtwoorden wil jij ?"))
-lengte = int(input("hoeveel lang moet uw wachtwoord zijn"))
-for x in range (1)3):
-                       for x in range (tekens):help
-                       
